476130cd4c3fa48885df6821dbf3f5156d6b4034
DJMaslek <realmaslek@gmail.com>
2019-12-18 21:09:38 +0700

    FC

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import NewsContainer from "./containers/NewsContainer";
+import LoginContainer from "./containers/LoginContainer";
+import ProfileContainer from "./containers/ProfileContainer";
+import StatusContainer from "./containers/StatusContainer";
+import Home from "./component/Home";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav className="navigation">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/news">News</Link>
+              </li>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <StatusContainer />
+
+          <Switch>
+            <Route path="/news">
+              <NewsContainer />
+            </Route>
+            <Route path="/login">
+              <LoginContainer />
+            </Route>
+            <Route path="/profile">
+              <ProfileContainer />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="*">
+              <NotFoundPage />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+function NotFoundPage() {
+  return <h2 className="header">NotFound</h2>;
 }
 
+/* const mapStateToProps = store => {
+  console.log(store);
+  return {
+    login: store.login,
+    profile: store.profile
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    handleLoginAction: (credentials, onSuccess, onFailure) =>
+      dispatch(handleLogin(credentials, onSuccess, onFailure)),
+    getProfileAction: () => dispatch(getProfile())
+  };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
+ */
 export default App;
