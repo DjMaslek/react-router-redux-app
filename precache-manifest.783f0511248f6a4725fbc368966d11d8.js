06abb5e65c43f71f72ab4d5e9aba2c530569d991
DJMaslek <realmaslek@gmail.com>
2019-12-18 21:08:50 +0700

    Updates

---
+++ precache-manifest.783f0511248f6a4725fbc368966d11d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85309241d37ec61a8b4c17df9f455112",
+    "url": "/react-router-redux-app/index.html"
+  },
+  {
+    "revision": "582a30906188a02c78e1",
+    "url": "/react-router-redux-app/static/css/main.ddfb4083.chunk.css"
+  },
+  {
+    "revision": "af2f3899c54c24c2d64e",
+    "url": "/react-router-redux-app/static/js/2.2faf929a.chunk.js"
+  },
+  {
+    "revision": "582a30906188a02c78e1",
+    "url": "/react-router-redux-app/static/js/main.092fdea9.chunk.js"
+  },
+  {
+    "revision": "fbfc1b14099fc227ba2c",
+    "url": "/react-router-redux-app/static/js/runtime-main.6dfaba28.js"
+  }
+]);
